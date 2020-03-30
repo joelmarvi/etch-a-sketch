2a959c4a5b6dc6bb59e0c48f5bb6f7626e9af2eb
Jorge Marzola <jorgemarzola@gmail.com>
2020-03-30 11:41:24 +0200

    Add comments and update README.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Etch-a-Sketch
+This project was created using HTML, CSS, and JavaScript.
 
 www.theodinproject.com
--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
-const grid = document.getElementById('gridBox');
+const grid = document.getElementById('grid-box');
 const dark = document.getElementById('dark');
 const newGrid = document.getElementById('new-grid');
 const clear = document.getElementById('clear');
 const color = document.getElementById('color');
 const userInput = prompt('How many rows and columns would you like?');
 
+//Modifying the grid functions
 function makeGrid(userInput) {
-    grid.style.setProperty('--gridDimension', userInput);
+    grid.style.setProperty('--grid-dimension', userInput);
     for(let i = 0; i < Math.pow(userInput, 2); i++) {
         let cell = document.createElement('div');
-        cell.classList.add('gridItem');
+        cell.classList.add('grid-item');
         grid.appendChild(cell);
     }
 }
 
-function darkColor(e) {
-    let cells = document.querySelectorAll('.gridItem');
-    cells.forEach(cell => cell.addEventListener('mouseover', function(e){
-        cell.style.backgroundColor = 'var(--hover-color)';
-    }));
-}
-
 function resizeGrid(e) {
+    //Eliminates all children from grid-box before creating a new grid
     while(grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
@@ -31,10 +26,19 @@ function resizeGrid(e) {
 }
 
 function clearGrid(e) {
-    let cells = document.querySelectorAll('.gridItem');
+    let cells = document.querySelectorAll('.grid-item');
     cells.forEach(cell => cell.style.backgroundColor = 'var(--start-color)');
 }
 
+//Coloring the grid
+function darkColor(e) {
+    let cells = document.querySelectorAll('.grid-item');
+    cells.forEach(cell => cell.addEventListener('mouseover', function(e){
+        cell.style.backgroundColor = 'var(--hover-color)';
+    }));
+}
+
+//Random color selected so it can be used by the function below
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -45,13 +49,16 @@ function getRandomColor() {
 }
 
 function randomColor(e) {
-    let cells = document.querySelectorAll('.gridItem');
+    let cells = document.querySelectorAll('.grid-item');
     cells.forEach(cell => cell.addEventListener('mouseover', function(e){
         cell.style.backgroundColor = getRandomColor();
     }));
 }
 
+//Sterting the grid
 makeGrid(userInput);
+
+//Event listeners
 dark.addEventListener('click', darkColor);
 newGrid.addEventListener('click', resizeGrid);
 clear.addEventListener('click', clearGrid);
